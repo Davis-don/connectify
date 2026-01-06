dcbc899943c20f8e394fe3b4a8f4f79d3ebe84c4
Davis-don <davismugoikou@gmail.com>
2026-01-06 21:26:18 +0300

    set token to expire in an year

--- server/connect/connect/settings.py
+++ server/connect/connect/settings.py
@@ -109,8 +109,8 @@ AUTH_USER_MODEL = 'users.User'
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),     # 24 hours
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=365),
 
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
